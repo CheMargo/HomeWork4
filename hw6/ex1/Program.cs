88f49abc31384bdb8e96f424f0cc942da8cfe605
Margarita Cherepanova <ritosnsk@gmail.com>
2023-01-08 20:21:27 +0700

    решение задачи

---
+++ hw6/ex1/Program.cs
@@ -0,0 +1,61 @@
+﻿/*Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+0, 7, 8, -2, -2 -> 2
+
+1, -7, 567, 89, 223-> 3*/
+
+// Console.Write("введите число: ");
+// int a = int.Parse(Console.ReadLine());
+
+// Console.Write("введите число: ");
+// int b = int.Parse(Console.ReadLine());
+
+// Console.Write("введите число: ");
+// int c = int.Parse(Console.ReadLine());
+
+// Console.Write("введите число: ");
+// int d = int.Parse(Console.ReadLine());
+
+// Console.Write("введите число: ");
+// int e = int.Parse(Console.ReadLine());
+
+
+
+
+
+
+int[] FillArray(int Length)
+{
+    int[] array = new int[Length];
+    int count = 0;
+    while (count < Length)
+    {
+        Console.Write($"введите {count+1} число: ");
+        array[count] = int.Parse(Console.ReadLine());
+        count++;
+    }
+    return array;
+}
+
+int PositiveNumberCount (int[] array)
+{
+    int count = 0;
+    for(int i = 0; i < array.Length; i++ )
+    {
+        if(array[i] > 0)
+        {
+            count++;
+        }
+    }
+    return count;
+}
+
+
+
+Console.Write("введите количество вводимых чисел: ");
+int Length = int.Parse(Console.ReadLine());
+int[] array = new int[Length];
+array = FillArray(Length);
+int result = PositiveNumberCount(array);
+
+Console.Write(result);
