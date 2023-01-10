f82c5ebbdbe8c538de52cf344eb15b2c2dc2ba37
Margarita Cherepanova <ritosnsk@gmail.com>
2023-01-10 17:23:04 +0700

    решение задач

---
+++ hw7/ex2/Program.cs
@@ -0,0 +1,73 @@
+﻿/*Задача 50. Напишите программу, которая на вход принимает позиции элемента в двумерном массиве, 
+и возвращает значение этого элемента или же указание, что такого элемента нет.
+
+Например, задан массив:
+
+1 4 7 2
+
+5 9 2 3
+
+8 4 2 4
+
+17 -> такого числа в массиве нет*/
+
+//за позицию 1,1 мы принимаем индекс [0,0]
+int ReadInt()
+    {
+        return int.Parse(Console.ReadLine());
+    }
+
+int[,] GetArray (int length, int secondLength)
+{
+    int[,] array = new int[length, secondLength];
+    Random random = new Random();
+    for (  int i = 0; i < length; i++)
+    {
+        for (int j = 0; j < secondLength; j++)
+        {
+            array[i, j] = random.Next(1000);
+        }
+    }
+    return array;
+}
+
+void GetArrayElement (int[,] array)
+{
+    Console.Write("введите индекс двумерного массива разделяя числа пробелом: ");
+    string[] num = Console.ReadLine().Split(' ');
+               int a = int.Parse(num[0]);
+               int b = int.Parse(num[1]);
+    
+    if(a > array.GetLength(0) | b > array.GetLength(1))
+    {
+        Console.WriteLine("такого числа в массиве нет");
+    }
+    else
+    {
+        Console.WriteLine($"'элемент массива с заданым индексои имеет значение: {array[a,b]}'");
+    }
+ 
+}
+
+void PrintArray (int[,] array)
+{
+     for (  int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write($"{array[i,j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+
+int firstLength = ReadInt();
+int secondLength = ReadInt();
+
+int[,] array = GetArray(firstLength, secondLength);
+
+PrintArray(array);
+System.Console.WriteLine();
+GetArrayElement(array);
+
---
+++ hw7/ex3/Program.cs
@@ -0,0 +1,68 @@
+﻿/*Задача 52. Задайте двумерный массив из целых чисел. Найдите среднее арифметическое элементов в каждом столбце.
+
+Например, задан массив:
+1 4 7 2
+5 9 2 3
+8 4 2 4
+Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3.
+*/
+
+int ReadInt()
+    {
+        return int.Parse(Console.ReadLine());
+    }
+
+int[,] GetArray (int length, int secondLength)
+{
+    int[,] array = new int[length, secondLength];
+    Random random = new Random();
+    for (  int i = 0; i < length; i++)
+    {
+        for (int j = 0; j < secondLength; j++)
+        {
+            array[i, j] = random.Next(1000);
+        }
+    }
+    return array;
+}
+
+string AVGColumn (int[,] array)
+{
+    string result = string.Empty;
+    for (  int column = 0; column < array.GetLength(1); column++)   //столбцы
+    {
+        double sum = 0;
+        for (int row = 0; row < array.GetLength(0); row++)          //строки
+        {
+            sum += array[row,column];
+        
+        }
+    result += sum / array.GetLength(0) + " ";
+    }
+    return result;
+ 
+}
+
+void PrintArray (int[,] array)
+{
+     for (  int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write($"{array[i,j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+
+int firstLength = ReadInt();
+int secondLength = ReadInt();
+
+int[,] array = GetArray(firstLength, secondLength);
+
+PrintArray(array);
+System.Console.WriteLine();
+string result = AVGColumn(array);
+
+System.Console.WriteLine(result);
