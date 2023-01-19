fa7f32787c3b402bb1ffbe95d7af169c07d85b4b
Margarita Cherepanova <ritosnsk@gmail.com>
2023-01-19 20:05:44 +0700

    задача 2

---
+++ hw9/ex2/Program.cs
@@ -0,0 +1,35 @@
+﻿/* Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+
+M = 1; N = 15 -> 120
+M = 4; N = 8. -> 30 */
+
+int ReadInt(string arg)
+{
+    Console.Write($"введите {arg} ");
+    return int.Parse(Console.ReadLine());
+}
+
+int SumOfNaturalNumbersRecurse(int m, int n)
+{
+    if (n == m)
+    {
+        return m;
+    }
+    if (n == (m + 1))
+    {
+        return (m + n);
+    }
+    return (m + n + SumOfNaturalNumbersRecurse(m + 1, n - 1));
+}
+
+
+int m = ReadInt("Введите число M");
+int n = ReadInt("Введите число N");
+if (m > n)
+{
+    Console.Write($"{m} больше {n}, нельзя посчитать!");
+}
+else
+{
+    Console.Write($"Сумма: {SumOfNaturalNumbersRecurse(m, n)}");
+}
